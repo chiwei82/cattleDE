e2131ae7f405ac4a3d3cebe0ec5371431ab6e87d
Tony <s481597@gmail.com>
2026-08-05 15:32:54 +0100

    upload task sh

---
+++ contactTask.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:rtx_3090:1
+#SBATCH --partition gpu
+#SBATCH --job-name=gpujob
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=32G
+#SBATCH --account=COSC021063
+#SBATCH --output=log/out/%j.out
+#SBATCH --error=log/err/%j.err
+
+set -euo pipefail
+
+cd /user/work/yx25778/cattleDE
+
+module load languages/python/3.12.3
+source venv/bin/activate
+
+python -m contactTest.train_contact
